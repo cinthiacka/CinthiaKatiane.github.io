f0ca2362273464cc65509b975fba8eb14b5bde5b
Cinthia Katiane <cinthia_katiane@hotmail.com>
2020-12-01 02:34:03 -0300

    frame

---
+++ src/views/AboutMe.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="p-0">
+    <Header/>
+    <section id="welcome" class='p-0 m-0 '>
+      <v-layout align-center column justify-start m-0 p-0>
+        <div class="mb-1 title">{{info.fullName}}</div>
+        <div class="subtitle"> {{info.snd_role}} </div>
+        <br>
+      </v-layout>  
+    </section>
+
+    <section id="work" class='p-0 m-0 welcome'>
+      <v-row>
+        <v-col align="center" justify="center">
+          <v-card class="card-float d-inline-block" width="85%" elevation="0">
+            <br>
+            <hr>
+            <div>
+              <div class="wrapper">
+              <div class="topic">{{info.aboutMe.title}}</div>
+              <div class="subtitle">
+                {{info.aboutMe.text}}
+              </div>
+              
+              <div class="topic">{{info.formation.title}}</div>
+              <div v-for="item in info.formation.courses" :key='item' class="subtitle">
+                {{item}}
+              </div>      
+
+              <div class="topic">{{info.skillSet.title}}</div>
+              <div v-for="item in info.skillSet.skills" :key='item' class="subtitle">
+                {{item}}
+              </div>      
+
+              <div class="topic">{{info.experience.title}}</div>
+              <div v-for="item in info.experience.experiences" :key='item' class="subtitle">
+                {{item}}
+              </div>      
+
+              </div>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+      <Footer/>
+    </section>
+    
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+
+  export default {
+    name: 'AboutMe',
+    components: {
+      Header,
+      Footer
+    },
+    data(){
+      return{
+        info: require("../../data.json"),
+      }
+    }
+  }
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/icon?family=Poppins");
+
+.welcome{
+  height: 90vh !important;
+}
+.title{
+  margin-top: 2em;
+  font-size: 3em!important;
+  font-family: Poppins, sans-serif !important;
+  font-weight: 900;
+  color: black!important;
+}
+.topic{
+  margin-top: 2em;
+  font-size: 2em!important;
+  font-family: Poppins, sans-serif !important;
+  font-weight: 900;
+  color: black!important;
+}
+.subtitle{
+  font-size: 1.2em;
+  font-family: Poppins, sans-serif;
+  font-weight: 50;
+  color: black;
+}
+.wrapper {
+  padding: 3em;
+  padding-top: 0;
+  margin: 0 auto;
+  @media only screen and (max-width: 799px) {
+    max-width: 100%;
+    margin: 0 1.5rem;
+  }
+}
+</style>
